5bb4bc1eb8fd1e6cb8344091a536920b16d4a5e7
Yannik Diskowski <yannik@diskowski.net>
2026-07-13 22:25:51 +0200

    added particles, border, closest point on line calculation

--- script.js
+++ script.js
@@ -14,8 +14,8 @@ resizeCanvas();
 
 class Player {
     constructor() {
-
         this.alpha = 0;
+        this.size
         this.rightInput = false;
         this.leftInput = false;
         document.addEventListener("keydown", (e) => {
@@ -26,30 +26,91 @@ class Player {
             if (e["key"] == "ArrowRight") this.rightInput = false;
             if (e["key"] == "ArrowLeft") this.leftInput = false;
         })
+        document.addEventListener("keydown", (e) => {
+            if (e["key"] == " ") {
+                particles.push(new Particle(canvasWidth/2, canvasHeight/2, 10*Math.sin(this.alpha), 10*Math.cos(this.alpha)));
+            }
+        })
+    }
+    step(dt) {
+        this.dAlpha = dt
+        this.alpha -= this.rightInput ? this.dAlpha : 0;
+        this.alpha += this.leftInput ? this.dAlpha : 0;
     }
     draw() {
-        
+        ctx.beginPath();
+        ctx.lineWidth = 2;
+        ctx.moveTo(canvasWidth/2 - 10*Math.sin(this.alpha), canvasHeight/2 - 10*Math.cos(this.alpha));
+        ctx.lineTo(canvasWidth/2 - 50*Math.sin(this.alpha), canvasHeight/2 - 50*Math.cos(this.alpha));
+        ctx.stroke();
+    }
+}
+
+class Particle {
+    constructor(x, y, vx, vy) {
+        this.x = x;
+        this.y = y;
+        this.vx = vx;
+        this.vy = vy;
+        this.size = 2;
+    }
+    step(dt) {
+        this.x += this.vx * dt;
+        this.y += this.vy * dt;
+
+        var cl = this.getClosestPointOnBorder(borders[0])
+        ctx.beginPath();
+        ctx.arc(cl[0], cl[1], this.size, 0, 2 * Math.PI);
+        ctx.fillStyle = "green";
+        ctx.fill();
+    }
+    getClosestPointOnBorder(border) {
+        const dx = border.x2 - border.x1
+        const dy = border.y2 - border.y1
+        var t = ((this.x - border.x1)*dx + (this.y - border.y1)*dy) / (dx*dx + dy*dy)
+        t = Math.max(0, Math.min(1, t))
+        return [border.x1 + t*dx, border.y1 + t*dy]
+    }
+    draw() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+        ctx.fillStyle = "red";
+        ctx.fill();
+    }
+}
+
+class Border {
+    constructor(x1, y1, x2, y2) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
+    draw() {
+        ctx.beginPath();
+        ctx.lineWidth = 2;
+        ctx.moveTo(this.x1, this.y1);
+        ctx.lineTo(this.x2, this.y2);
+        ctx.stroke();
     }
 }
 
 
-borders = []
-b = [100, 100, 200, 400]
-borders.push(b)
+borders = [new Border(canvasWidth/2 -100, canvasHeight/2 +100, canvasWidth/2 +100, canvasHeight/2 + 50)]
+particles = []
 
-var angle = 0;
+player = new Player()
 function main(dt) {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    dAngle = dt
-    angle -= rightInput ? dAngle : 0;
-    angle += leftInput ? dAngle : 0;
-
-    ctx.beginPath();
-    ctx.lineWidth = 2;
-    ctx.moveTo(canvasWidth/2 - 10*Math.sin(angle), canvasHeight/2 - 10*Math.cos(angle));
-    ctx.lineTo(canvasWidth/2 - 50*Math.sin(angle), canvasHeight/2 - 50*Math.cos(angle));
-    ctx.stroke()
+    player.step(dt)
+    player.draw()
+    for (var b of borders) {
+        b.draw()
+    }
+    for (var p of particles) {
+        p.step(dt) 
+        p.draw()
+    }
 }
 
 lastTime = 0;
