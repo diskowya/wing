e797bee88f75e53636d3cc14024eab7d3615dad4
Yannik Diskowski <yannik@diskowski.net>
2026-07-18 18:36:09 +0200

    completed border collision

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
+const cos = Math.cos;
+const sin = Math.sin;
+const PI = Math.PI;
+
 var canvasWidth, canvasHeight; 
 function resizeCanvas() {
     const canv = document.getElementById("canvas");
@@ -18,10 +22,12 @@ class Player {
         this.y = canvasHeight/2;
         this.vx = 0;
         this.vy = 0;
-        this.dAlpha = 0;
         this.alpha = 0;
-        this.thrust = 4;
+        this.radius = 10;
+        this.dAlpha = 0;
+        this.thrust = 1;
         this.speed_limit = 10;
+
         this.rightInput = false;
         this.leftInput = false;
         document.addEventListener("keydown", (e) => {
@@ -32,11 +38,6 @@ class Player {
             if (e.key === "ArrowRight") this.rightInput = false;
             if (e.key === "ArrowLeft") this.leftInput = false;
         });
-        //document.addEventListener("keydown", (e) => {
-        //    if (e["key"] == " ") {
-        //        particles.push(new Particle(canvasWidth/2, canvasHeight/2, 10*Math.sin(this.alpha), 10*Math.cos(this.alpha)));
-        //    }
-        //})
     }
     step(dt) {
         dt = !isNaN(dt) ? dt : 0;
@@ -49,24 +50,90 @@ class Player {
         this.alpha += 0.3*this.dAlpha*dt;
         
         if (this.vx < this.speed_limit) this.vx -= this.thrust*Math.sin(this.alpha) * dt;
-        if (this.vx < this.speed_limit)this.vy -= this.thrust*Math.cos(this.alpha) * dt;
+        if (this.vx < this.speed_limit) this.vy -= this.thrust*Math.cos(this.alpha) * dt;
+
+        const collision = borders[0].check_collision(this.x, this.y, 10)
+        if (collision) {
+            [this.vx, this.vy] = this.perform_collision(collision[0], collision[1], this.vx, this.vy);
+        }
+
         this.x += this.vx;
         this.y += this.vy;
 
     }
+    perform_collision(l_x_norm, l_y_norm, vx, vy) {
+        const v_b_x = l_x_norm*vx + l_y_norm*vy;
+        const v_b_y = l_y_norm*vx - l_x_norm*vy;
+        const vx_c = l_x_norm*v_b_x - l_y_norm*v_b_y;
+        const vy_c = l_y_norm*v_b_x + l_x_norm*v_b_y;
+        return [vx_c, vy_c];
+    }
     draw() {
         const x = this.x;
         const y = this.y;
-        const alpha = this.alpha;
-        const scale = 6;
+        const a = this.alpha;
+        const s = 6;
+        const c = 1.5;
+
+        const s_sin_a = s*sin(a);
+        const s_cos_a = s*cos(a);
+        const s_c_sin_a = c*s_sin_a;
+        const s_c_cos_a = c*s_cos_a;
 
         ctx.beginPath();
-        ctx.arc(x, y, 0.8*scale, Math.PI - alpha, -alpha);
-        ctx.lineTo(x + 2*scale*Math.cos(alpha) , y - 2*scale*Math.sin(alpha));
-        ctx.lineTo(x - 5*scale*Math.sin(alpha), y - 5*scale*Math.cos(alpha));
-        ctx.lineTo(x - 2*scale*Math.cos(alpha), y + 2*scale*Math.sin(alpha));
-        ctx.lineTo(x - 0.8*scale*Math.cos(alpha), y + 0.8*scale*Math.sin(alpha));
+        ctx.arc(x + s_c_sin_a, y + s_c_cos_a, 0.8*s, PI - a, -a);
+        ctx.lineTo(x + 2*s_cos_a + s_c_sin_a, y - 2*s_sin_a + s_c_cos_a);
+        ctx.lineTo(x - 5*s_sin_a + s_c_sin_a, y - 5*s_cos_a + s_c_cos_a);
+        ctx.lineTo(x - 2*s_cos_a + s_c_sin_a, y + 2*s_sin_a + s_c_cos_a);
+        ctx.lineTo(x - 0.8*s_cos_a + s_c_sin_a, y + 0.8*s_sin_a + s_c_cos_a);
         ctx.fill();
+    }
+}
+
+class Border {
+    constructor(x1, y1, x2, y2) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+
+        this.l_x = this.x2 - this.x1;
+        this.l_y = this.y2 - this.y1;
+        this.length = Math.sqrt(this.l_x*this.l_x + this.l_y*this.l_y);
+        this.l_x_norm = this.l_x / this.length;
+        this.l_y_norm = this.l_y / this.length;
+    }
+    check_collision(x, y, threshold) {
+        const p_x = x - this.x1;
+        const p_y = y - this.y1;
+        var t = (p_x*this.l_x + p_y*this.l_y) / (this.l_x*this.l_x + this.l_y*this.l_y);
+        t = Math.max(0, Math.min(1, t));
+        const p_l_x = this.x1 + t*this.l_x; 
+        const p_l_y = this.y1 + t*this.l_y; 
+        const dx = x - p_l_x;
+        const dy = y - p_l_y;
+        const dist = Math.sqrt(dx*dx + dy*dy);
+
+        if (dist > threshold) return false;
+        if (0 < t && t < 1) return [this.l_x_norm, this.l_y_norm];
+        
+        if (t == 0) {
+            return [p_y/dist, -p_x/dist]
+        }
+
+        if (t == 1) {
+            const p_x_2 = x - this.x2;
+            const p_y_2 = y - this.y2;
+            return [p_y_2/dist, -p_x_2/dist]
+        }
+
+    }
+    draw() {
+        ctx.beginPath();
+        ctx.lineWidth = 2;
+        ctx.moveTo(this.x1, this.y1);
+        ctx.lineTo(this.x2, this.y2);
+        ctx.stroke();
     }
 }
 
@@ -101,22 +168,6 @@ class Particle {
         ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
         ctx.fillStyle = "red";
         ctx.fill();
-    }
-}
-
-class Border {
-    constructor(x1, y1, x2, y2) {
-        this.x1 = x1;
-        this.y1 = y1;
-        this.x2 = x2;
-        this.y2 = y2;
-    }
-    draw() {
-        ctx.beginPath();
-        ctx.lineWidth = 2;
-        ctx.moveTo(this.x1, this.y1);
-        ctx.lineTo(this.x2, this.y2);
-        ctx.stroke();
     }
 }
 
